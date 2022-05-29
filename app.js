38a17f0d030c4cceb165547729c5fb9f891f6113
ShaylinTRK <>
2022-05-29 17:13:15 +0000

    added better file handling to routing

--- app.js
+++ app.js
@@ -1,11 +1,31 @@
 const express = require("express")
 const app = express()
 
-app.use(express.static("public"))
+app.use(express.static('./public/styles'));
+app.use(express.static('./public/imgs'));
 
-app.get("/", function (req, res) {
-  res.send("<h1>Hello World!</h1>")
+app.get(["/", '/home', '/index'], (req, res) => {
+  const options = {root: './public'}
+  res.sendFile('/html/home.html', options);
+});
+
+app.get("/contact", (req, res) => {
+  const options = {root: './public'}
+  res.sendFile('/html/contact.html', options);
+});
+
+app.get("/robots.txt", (req, res) => {
+  const options = {root: './public'}
+  res.sendFile('/meta/robots.txt', options);
+});
+
+app.use((req, res) => {
+  res.status(404).send("<h1> 404: Page not found! </h1>");
+});
+
+app.use((err, req, res, next) => {
+  console.error(err.stack)
+  res.status(500).send('500: Oh no! Something\'s gone wrong...')
 })
 
-app.listen(process.env.PORT || 3000, 
-	() => console.log("Server is running..."));
+app.listen(process.env.PORT || 3000, () => console.log("Server is running..."));
