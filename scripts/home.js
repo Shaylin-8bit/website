5fd69a372d94de9bab2c93dcf237d347542abf06
Shaylin-8bit <malakai.8bit@outlook.com>
2022-08-12 20:24:39 -0500

    Updated fading effect for top nav

--- scripts/home.js
+++ scripts/home.js
@@ -23,27 +23,14 @@ const scrollToId = (targetId) => {
 $(document).ready(function() {
     const checkMainSection = () => {
         const mainContent = $('#main-content');
+        const opacity = mainContent.scrollTop() / $('#mini-bio').height()
+        const click = opacity > .5 ? 'all' : 'none'
+
+        $('#header-pfp-wrapper').css('opacity', opacity);
+        $('#header-pfp-wrapper').css('pointer-events', 'none');
+        $('#header-nav').css('opacity', opacity);
+        $('#header-nav').css('pointer-events', 'none');
         
-        $('.main-section').each((i, obj) => {
-            // hacky scroll fix. Should be updated later.
-            //  manditory scroll is often off by one pixel. scrollToId used to fix that
-            const point = obj.offsetTop - obj.parentElement.offsetTop;
-            const lst = [point-1, point, point+1];
-            if (lst.includes(mainContent.scrollTop())) {
-                scrollToId(obj.id);
-                if (obj.id != 'mini-bio') {
-                    $('#header-pfp-wrapper').animate({'opacity': '100%'}, 500);
-                    $('#header-pfp-wrapper').css('pointer-events', 'all');
-                    $('#header-nav').animate({'opacity': '100%'}, 500);
-                    $('#header-nav').css('pointer-events', 'all');
-                } else {
-                    $('#header-pfp-wrapper').animate({'opacity': '0%'}, 500);
-                    $('#header-pfp-wrapper').css('pointer-events', 'none');
-                    $('#header-nav').animate({'opacity': '0%'}, 500);
-                    $('#header-nav').css('pointer-events', 'none');
-                }
-            }
-        });
     };
 
     checkMainSection();
