aa3616c239e3b6d49d2e5a9d8ea354411d1c0eaa
Shaylin-8bit <malakai.8bit@outlook.com>
2022-08-15 18:31:29 -0500

    add route projects/articles/admin

--- routes/projects.js
+++ routes/projects.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.render('base', {text: 'articles page'});
+    res.render('base', {text: 'projects page'});
 });
 
 module.exports = router;
